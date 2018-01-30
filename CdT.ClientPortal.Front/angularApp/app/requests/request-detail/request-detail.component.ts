931251e80b75f6e46a9698637e0cf6cf5035102c
Samuel BERTHELOT <BERTHSA@communication1.ntld>
2018-01-30 17:42:37 +0100

    request details multiselect ordering

--- CdT.ClientPortal.Front/angularApp/app/requests/request-detail/request-detail.component.ts
+++ CdT.ClientPortal.Front/angularApp/app/requests/request-detail/request-detail.component.ts
@@ -1,9 +1,13 @@
+import { EntityManagerService } from './../../entity-manager.service';
 import { Observable } from 'rxjs/Observable';
-import { Purpose, Department, DeliveryMode } from './../../model/entity-model';
+import { Purpose, Department, DeliveryMode, Request } from './../../model/entity-model';
 import { RequestService } from './../requests.service';
 import { ViewChild, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import * as _ from 'lodash';
+import { Contact } from '../../model/contact';
+import { EntityState } from 'breeze-client';
 
 @Component({
   selector: 'app-request-detail',
@@ -19,11 +23,14 @@ export class RequestDetailComponent implements OnInit {
   public deliveryModes: DeliveryMode[];
   public templates: any[];
   public filteredTemplateList: any[];
+  public activeContacts: Contact[];
+  public selectedContacts: Contact[];
+  public selectedRecipients: Contact[];
 
   @ViewChild('requestForm') requestForm: NgForm;
   @ViewChild("templateList") templateList;
 
-  constructor(private _requestService: RequestService, private _route: ActivatedRoute, private _router: Router) {
+  constructor(private _requestService: RequestService, private _entityManagerService: EntityManagerService, private _route: ActivatedRoute, private _router: Router) {
     //this.filteredTemplateList = this.templates.slice();
   }
 
@@ -35,6 +42,21 @@ export class RequestDetailComponent implements OnInit {
     this.deliveryModes = this._requestService.getDeliveryModes();
     this.templates = this._requestService.getRequestTemplates().map(t => { return { text: t.templateName, value: t.id } });
     this.filteredTemplateList = this.templates.slice();
+    this.activeContacts = _.filter(this.request.client.contacts, { 'isActive': true });
+    this.selectedContacts = [];
+    this.selectedRecipients = [];
+
+    this.request.requestContacts.forEach((reqContact) => {
+      if (reqContact.entityAspect.entityState !== EntityState.Deleted) {
+        this.selectedContacts.push(reqContact.contact);
+      }
+    });
+
+    this.request.requestDeliveryContacts.forEach((rdc) => {
+      if (rdc.entityAspect.entityState !== EntityState.Deleted) {
+        this.selectedRecipients.push(rdc.contact);
+      }
+    });
   }
 
   /**
@@ -50,6 +72,10 @@ export class RequestDetailComponent implements OnInit {
 
   onSave(): void {
     if (this.requestForm.invalid) return;
+
+    this._entityManagerService.checkMany2ManyModifications('RequestContact', this.request, this.selectedContacts, this.request.requestContacts, 'request', 'contact', false);
+    this._entityManagerService.checkMany2ManyModifications('RequestDeliveryContact', this.request, this.selectedRecipients, this.request.requestDeliveryContacts, 'request', 'contact', false);
+
     this._requestService.save().then(() => {
       this._router.navigate(['requests']);
     }, error => {
@@ -58,5 +84,10 @@ export class RequestDetailComponent implements OnInit {
         //this.errorMessage = error.message;
       }
     });
+  }
+  
+  canSave(): boolean{
+     /*!vm.sourceLanguagesChanged && !vm.many2manyHasChanged && !hasChanges()) || hasErrors() */
+     return  this._entityManagerService.hasChanges();
   }
 }
