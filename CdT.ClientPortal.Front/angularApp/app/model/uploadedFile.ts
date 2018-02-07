6a05097a64681d6802de49ef30a1d7412ebde5f3
graphicsxp <graphicsxp@gmail.com>
2018-02-07 19:34:00 +0100

    fixed tslint

--- CdT.ClientPortal.Front/angularApp/app/model/uploadedFile.ts
+++ CdT.ClientPortal.Front/angularApp/app/model/uploadedFile.ts
@@ -1,11 +1,12 @@
 export class UploadedFile {
+
+    public path: String;
+    public name: String;
+    public size: number;
+
     constructor(path: String, name: String, size: number) {
         this.path = path;
         this.name = name;
         this.size = size;
     }
-
-    public path: String;
-    public name: String;
-    public size: number;
 }
